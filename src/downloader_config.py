c3f917bd18d709a6db6ee1f3a1d2bcf5a0662144
poslogithub <black_tp@hotmail.com>
2023-11-04 07:17:13 +0900

    バージョンあげ

--- src/downloader_config.py
+++ src/downloader_config.py
@@ -15,7 +15,7 @@ class DownloaderConfigFile(ConfigFile):
     def __init__(self, path=None):
         super().__init__(path)
         self.config = {
-            ConfigKey.URL: "https://magic.wizards.com/ja/news/card-image-gallery/phyrexia-all-will-be-one",
+            ConfigKey.URL: "https://magic.wizards.com/ja/products/the-lost-caverns-of-ixalan/card-image-gallery",
             ConfigKey.FILE: "",
             ConfigKey.FILE_DIR: expanduser('~')+"\Downloads",
             ConfigKey.DIR: expanduser('~'),
